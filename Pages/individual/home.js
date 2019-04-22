7557429f7142bb9dcd21f4ebd8a13030cc6b795c
김보성 <ghalsru@naver.com>
2019-04-23 04:23:11 +0900

    Change : 글 수정

--- Pages/individual/home.js
+++ Pages/individual/home.js
@@ -1,22 +1,37 @@
 var Observable = require('FuseJS/Observable');
 
-var test = Observable();
 var title = Observable();
 var content = Observable();
-var expense = Observable();
+var expense = 1200000;
+var pin = 0;
+
 
 exports.items = Observable()
 exports.isLoading = Observable(false);
 
-function loadSome() {
+function addComma(num) {
+  var regexp = /\B(?=(\d{3})+(?!\d))/g;
+  return num.toString().replace(regexp, ',');
+}
 
-	for (var i=0; i < 5; ++i ) {
+function loadSome() {
+	// for (var i=0; i < 5; ++i ) {
 	
-			exports.items.add( {
-				title : "공모전 수상",
-				content : "ROS",
-				expense : 300000,
+	// 		exports.items.add( {
+	// 			title : "한신포차",
+	// 			content : "주 3회근무",
+	// 			expense : addComma(expense),
+			
+	// 		})
+	// }
+	if(pin == 0){
+		exports.items.add( {
+				title : "한신포차",
+				content : "주 3회근무",
+				expense : addComma(expense),
+			
 			})
+		pin = 1;
 	}
 	
 	exports.isLoading.value = false
@@ -31,6 +46,10 @@ var minSimulatedDelay = 0.25
 exports.loadMore = function() {
 	//it's possible this gets called again before the previous loading is complete
 	if (exports.isLoading.value) {
+		return
+	}
+	
+	if(pin == 1){
 		return
 	}
 	exports.isLoading.value = true
